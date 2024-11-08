97a6c578d1ca1ec8cb1435ef05e2da8a7b9c7048
harshit <harshitpurohit@gkmit.co>
2024-11-08 11:08:07 +0530

    feat: [sc-4] add report model and migration

---
+++ src/models/Report.js
@@ -0,0 +1,56 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Report extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    // static associate(models) {
+    //   // define association here
+    // }
+  }
+  Report.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      user_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      group_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: 'groups',
+          key: 'id',
+        },
+      },
+      report_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      generated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      updated_at: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: 'Report',
+      paranoid: true,
+      timestamps: true,
+    },
+  );
+  return Report;
+};
