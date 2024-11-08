4105df4f15945c156de9b45591cade33cfc61293
harshit <harshitpurohit@gkmit.co>
2024-11-08 07:46:04 +0530

    feat: [sc-4] add users_roles model and migration

---
+++ src/migrations/20241107091088-create-user-role.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users_roles', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+        allowNull: false,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      role_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'roles',
+          key: 'id',
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('users_roles');
+  },
+};
---
+++ src/models/UserRole.js
@@ -0,0 +1,46 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class UserRole extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    // static associate(models) {
+    //   // define association here
+    // }
+  }
+  UserRole.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      user_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      role_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: 'roles',
+          key: 'id',
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'UserRole',
+      timestamps: true,
+      underscored: true,
+    },
+  );
+  return UserRole;
+};
