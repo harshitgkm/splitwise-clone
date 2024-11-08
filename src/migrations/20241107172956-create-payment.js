d5214f5b4265dbe61162c563c7603c9e4c8ede40
harshit <harshitpurohit@gkmit.co>
2024-11-08 10:53:07 +0530

    feat: [sc-4] add payment model and migration

---
+++ src/migrations/20241107172956-create-payment.js
@@ -0,0 +1,65 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('payments', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+        allowNull: false,
+        primaryKey: true,
+      },
+      group_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'groups',
+          key: 'id',
+        },
+      },
+      payer_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      payee_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      amount: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.ENUM('Pending', 'Completed', 'Failed'),
+        allowNull: false,
+        defaultValue: 'Pending',
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE,
+        defaultValue: null,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('payments');
+  },
+};
