37d4293c23de3a3d438f926578fbea35b6d14b94
harshit <harshitpurohit@gkmit.co>
2024-11-08 08:05:47 +0530

    feat: [sc-4] add association between users and roles

--- src/models/Role.js
+++ src/models/Role.js
@@ -7,9 +7,12 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    // static associate(models) {
-    //   // define association here
-    // }
+    static associate(models) {
+      Role.belongsToMany(models.User, {
+        through: models.UserRole,
+        foreignKey: 'role_id',
+      });
+    }
   }
   Role.init(
     {
--- src/models/User.js
+++ src/models/User.js
@@ -7,9 +7,12 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    // static associate(models) {
-    //   // define association here
-    // }
+    static associate(models) {
+      User.belongsToMany(models.Role, {
+        through: models.UserRole,
+        foreignKey: 'user_id',
+      });
+    }
   }
   User.init(
     {
