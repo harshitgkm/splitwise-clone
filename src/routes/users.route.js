142396b960e0cb159d3c08d9cb393088cf9b5335
harshit <harshitpurohit@gkmit.co>
2024-11-13 12:30:33 +0530

    feat: [sc-15] configure multer middleware for updating user profile

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -10,11 +10,18 @@ const {
 
 const { verifyToken } = require('../middlewares/auth.middleware.js');
 
+const upload = require('../middlewares/multer.middleware.js');
+
 const router = express.Router();
 
 router.get('/me', verifyToken, getUserProfile);
 
-router.put('/me', verifyToken, updateUserProfile);
+router.put(
+  '/me',
+  verifyToken,
+  upload.single('profile_image'),
+  updateUserProfile,
+);
 
 router.get('/outstanding-balance', verifyToken, getOutstandingBalance);
 
