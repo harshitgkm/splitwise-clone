f2849aaef10bcca892308a2976e2dc19cc77760b
harshit <harshitpurohit@gkmit.co>
2024-11-14 19:05:00 +0530

    fix: [sc-18] update redis setup

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,16 @@
 const { createClient } = require('redis');
-const redisClient = createClient();
+const dotenv = require('dotenv');
+dotenv.config({ path: __dirname + '/../../.env' });
+
+const redisHost = process.env.REDIS_HOST || 'localhost';
+const redisPort = process.env.REDIS_PORT || 6379;
+
+const redisClient = createClient({
+  url: `redis://${redisHost}:${redisPort}`,
+});
 
 redisClient.on('error', err => {
-  console.log('Error connecting redis client', err);
+  console.log('Error connecting Redis client:', err);
 });
 
 redisClient.connect();
