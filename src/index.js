603aa7ace9ec395d4b801b4fbfe50e7f404dc26f
harshit <harshitpurohit@gkmit.co>
2024-11-17 22:44:49 +0530

    feat: [sc-24] configure all swagger files in index.js and swagger.yaml

--- src/index.js
+++ src/index.js
@@ -2,6 +2,53 @@ const express = require('express');
 const { sequelize } = require('./models');
 const app = express();
 const { registerRoutes } = require('./routes');
+const path = require('path');
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+// const swaggerDocument = YAML.load(
+//   path.join(__dirname, 'swagger', 'auth.swagger.yaml'),
+// );
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// const swaggerDocument = YAML.load('src/swagger/swagger.yaml');
+const swaggerMain = YAML.load(path.join(__dirname, 'swagger', 'swagger.yaml'));
+
+// Load the auth and users swagger files
+const authSwagger = YAML.load(
+  path.join(__dirname, 'swagger', 'auth.swagger.yaml'),
+);
+const usersSwagger = YAML.load(
+  path.join(__dirname, 'swagger', 'users.swagger.yaml'),
+);
+const groupsSwagger = YAML.load(
+  path.join(__dirname, 'swagger', 'groups.swagger.yaml'),
+);
+const expensesSwagger = YAML.load(
+  path.join(__dirname, 'swagger', 'expenses.swagger.yaml'),
+);
+
+// Merge the Swagger files manually
+const combinedSwagger = {
+  ...swaggerMain,
+  paths: {
+    ...swaggerMain.paths,
+    ...authSwagger.paths,
+    ...usersSwagger.paths,
+    ...groupsSwagger.paths,
+    ...expensesSwagger.paths,
+  },
+  components: {
+    ...swaggerMain.components,
+    ...authSwagger.components,
+    ...usersSwagger.components,
+    ...groupsSwagger.components,
+    ...expensesSwagger.components,
+  },
+};
+
+// Serve Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(combinedSwagger));
 
 app.use(express.json());
 registerRoutes(app);
