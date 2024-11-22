5526ff28509cf455d5e62aa91a1b1dd5172369f5
harshit <harshitpurohit@gkmit.co>
2024-11-22 09:11:32 +0530

    feat: [sc-34] update eslint config and .gitignore file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,17 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import js from '@eslint/js';
+
 export default [
 	{ files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-	{ languageOptions: { globals: globals.node } },
+	{ ignores: ['temp.js', 'src/migrations/*', 'src/models/*'] },
+	{ languageOptions: { globals: globals.browser } },
 	pluginJs.configs.recommended,
+	js.configs.recommended,
+	{
+		rules: {
+			'no-unused-vars': 'warn',
+			'no-undef': 'warn',
+		},
+	},
 ];
