3fa04a74a5f80ccb142a2704d5b9bb62b36a7080
harshit <harshitpurohit@gkmit.co>
2024-11-26 10:40:18 +0530

    fix: [sc-43] send otp to user on registration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -26,7 +26,8 @@ const register = async (req, res) => {
       EX: 1800,
     });
 
-    res.json({ message: 'redirect to request-otp' });
+    await requestOtpService(email);
+    return res.json({ message: 'OTP sent to your email for registration' });
   } catch (err) {
     res.json({ error: err.message });
   }
